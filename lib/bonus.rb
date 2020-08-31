3d98cfaeba148f2baad7a922fe2c5d2facd528a2
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-31 04:32:04 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,36 +1,3 @@
-Skip to content
-Why GitHub? 
-Team
-Enterprise
-Explore 
-Marketplace
-Pricing 
-Search
-
-Sign in
-Sign up
-alexisadorn
-/
-Flatiron-Procedural-Ruby
-2123
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-Join GitHub today
-GitHub is home to over 50 million developers working together to host and review code, manage projects, and build software together.
-
-Flatiron-Procedural-Ruby/building-nested-hashes-v-000/lib/bonus.rb /
-@alexisadorn
-alexisadorn Adding new labs
-Latest commit 38f17da on Mar 28, 2017
- History
- 1 contributor
-30 lines (27 sloc)  995 Bytes
-  
 def bonus
   epic_tragedy = {
    :montague => {
@@ -41,35 +8,22 @@ def bonus
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
       ]
-   },
+   }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"},
+          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
   }
 
   #code your solution here:
-  epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:capulet][:heroine][:status] = "deadl"
-
+epic_tragedy[:montague][:patriarch][:name]
+  
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
